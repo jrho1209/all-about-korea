a41a6aa394603dfb6eb3faf4e6fe026f811aadf4
James Rho <jimmystac@gmail.com>
2025-09-24 22:26:33 +0900

    fixing deploy

--- src/sanity/client.js
+++ src/sanity/client.js
@@ -1,8 +1,8 @@
 import { createClient } from "@sanity/client";
 
 export const sanityClient = createClient({
-  projectId: process.env.SANITY_PROJECT_ID,      // Sanity 관리 페이지에서 확인
-  dataset: process.env.SANITY_DATASET,           // 예: "production"
+  projectId: process.env.SANITY_PROJECT_ID || "oi8lcd7v",      // Sanity 관리 페이지에서 확인
+  dataset: process.env.SANITY_DATASET || "production",           // 예: "production"
   apiVersion: "2023-01-01",          // 또는 최신 날짜
   useCdn: false,
 });
