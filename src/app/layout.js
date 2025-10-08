e3038f1f88f2d31dcdb90181caa1eed820a806be
James Rho <jimmystac@gmail.com>
2025-10-09 06:09:02 +0900

    font fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,7 @@
 import SessionProviderWrapper from "./components/Providers/SessionProviderWrapper";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Daejeon Travel Platform",
@@ -34,10 +23,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="antialiased">
         <SessionProviderWrapper>
           <Navbar />
-          <div className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <div>
             {children}
           </div>
           <Footer />
